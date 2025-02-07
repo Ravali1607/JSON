c1c1c5c95c43d58c18f2dde90757be4a9ff9ad56
Ravali Chakka <ravalichakka16@gmail.com>
2025-02-07 14:29:36 +0000

    commit

--- webapp/controller/jsonData.controller.js
+++ webapp/controller/jsonData.controller.js
@@ -17,15 +17,13 @@ sap.ui.define([
             var allProducts = oModel.getProperty("/ProductCollection");
             var filteredProducts = allProducts.filter(product=> product.ProductId === selectedKey);
             oModel.setProperty("/ProductCollection",filteredProducts);
-            that.getView().setModel().refresh(true);
-
+        } 
         //     var oModel = new sap.ui.model.json.JSONModel({
         //         ProductCollection : filteredProducts
         //     })
         // that.byId("ProductData").setModel(oModel);
         // that.getView().setModel(oModel);
         // that.clearSelection();
-        },
         // categorySearch: function(oEvent){
         //     var fieldSearch = oEvent.getSource();
         //     var result = fieldSearch.getBindingContext();
